3f20124d5d206a92f419f1b8522aa0b09618e4eb
norahammenberg <nora_kerstin@hotmail.com>
2023-09-21 14:06:09 +0100

    adding get element by id

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 //https://jacintodesign.github.io/quotes-api/data/quotes.json
 
+//we creating a const for every ID target:
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const autherText = document.getElementById('auther');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBTN = document.getElementById('new-quote');
+
+
 //a global variable is declared with let as its value will change:
-let apiQoutes = [];
+let apiquotes = [];
+
+//new quote function that randomly generates a new quotes.
+function newQuote() {
+   //we gonna make a dynamic random  function, we gonna use math.random
+   //math random returns a random number between 0-1
+   //math floor allowes os to return the whole int, no deciimals
+   // pick a random quote from teh api array:
+   //we do the same as apiQuote[1] but instead of the one we replace it with what will randomize a number
+   //by wrapping math.random() with math floor we automatically get a intergi wit out any decimals. 
+   //the we * with the lenght of the api array. Meaning we will never get an number that is larger the the length of the array.
+   const quote = apiquotes[Math.floor(Math.random() * apiquotes.length)];
+   //we will populat the auther and quote element from the random picked quote and save it in the const fro txt and quther:
+   //textCOntent let us save the content in a string and we = this const to quote: the random picked quote from the arry. auther because we only like the auther or text. JSON
+
+   autherText.textContent = quote.author;
+   console.log(quote);
+   quoteText.textContent = quote.text;
+}
 
 //Fetching quotes from an API:
 //async can run at anytime and it eill not interfear with the browesrer loading the page: 
@@ -14,9 +40,10 @@ async function getQuotes() {
         //the const response will only be used after it been filled with a response, if we donät do that itt will cause an error.
         const response = await fetch(apiUrl);
 
-        //the api response is saved in the varible apiQoutes and is being converted to json data. 
-        apiQoutes = await response.json();
-        console.log(apiQoutes);
+        //the api response is saved in the varible apiquotes and is being converted to json data. 
+        apiquotes = await response.json();
+        //calling the function newQuote that will generate a random quote:
+        newQuote();
     }
     //if this do not work we can catch any arrors here:
     catch (error) {
@@ -25,6 +52,7 @@ async function getQuotes() {
     }
     //
 }
+
 
 // on load of the page prun the function.
 getQuotes();
